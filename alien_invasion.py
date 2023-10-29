2d2bc0193681c7d3cd2e4302cede8b23feb7680e
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-29 20:33:28 +0100

    Subiendo la clase con la configuracion inicial de pantalla

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from Proyectos.Alien_Invasion.settings import Settings
+
+
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportamiento del juego"""
 
     def __init__(self):
         """Inicializa el juego y crea recursos"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height
+        ))
         pygame.display.set_caption("Alien Invasion")
+
 
     def run_game(self):
         """Inicia le bucle principal para el juego"""
@@ -19,6 +26,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redibuja la pantalla en cada paso por el bucle
+            self.screen.fill(self.settings.bg_color)
 
             # Hace visible la ultima pantalla dibujada
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Una clase para guardar toda la configuracion de Alien Invasion"""
+
+    def __init__(self):
+        """Inicializa la configuracion del juego"""
+        # Configuracion de pantalla
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
