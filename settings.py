00d81baaf05419ff8ecfd23d8d2ec993da364716
Antonio Ruiz <3speedemon3@gmail.com>
2023-11-01 16:59:57 +0100

    Creando las balas para la nave

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # Configuracion de la nave
         self.ship_speed = 1.5 # Valocidad inicial a la que se movera la nave (en 1,5 pixeles)
+
+        # Configuracion de las balas
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
